b1ea25ee06d404a9219001761cc1c3da58066eba
Laura León <lauraleon@192.168.1.5>
2026-03-03 08:48:42 -0500

    corrige test eliminar bot

--- tests/10_test_eliminar_bot.py
+++ tests/10_test_eliminar_bot.py
@@ -1,13 +1,12 @@
 from playwright.sync_api import Page, expect
 
-def test_eliminar_bot_chattigo(page: Page):
+def test_eliminar_bot(page: Page):
     page.get_by_role("textbox", name="Buscar Bot...").fill("laura")
     
-    page.locator("div.card-bot").filter(has_text="laura").get_by_role("button").click()
+    page.get_by_role("button").nth(3).click()
 
     page.get_by_role("paragraph").filter(has_text="Eliminar proyecto").click()
 
     page.get_by_role("button", name="Aceptar").click()
 
-    success_message = page.get_by_text("Se ha eliminado el bot")
-    expect(success_message).to_be_visible()
+    expect(page.get_by_text("Se ha eliminado el bot")).to_be_visible()
