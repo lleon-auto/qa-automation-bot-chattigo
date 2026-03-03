08f20d4337d15214b8b83855336fe66d3d212b12
Laura León <lauraleon@192.168.1.5>
2026-03-03 08:56:44 -0500

    corrige test importar bot

--- tests/08_test_importar_bot.py
+++ tests/08_test_importar_bot.py
@@ -7,7 +7,7 @@ def test_importar_bot_chattigo(page: Page):
     page.get_by_title('Importar proyecto').click()
     
     with page.expect_file_chooser() as fc_info:
-        page.get_by_role('button', { 'name': 'Cargar' }).click()
+        page.get_by_role('button', name='Cargar').click()
     
     file_chooser = fc_info.value
     file_chooser.set_files('bot-automation-flujos Copia(GENERAL).json')
